bac5e1dc74af2aee94dd46fed0cc33e0ff14bb5d
Aswathy Mohan L <aswathymohan.troodon@outlook.com>
2019-07-29 14:35:48 +0530

    qr code

--- backend/server.js
+++ backend/server.js
@@ -17078,6 +17078,31 @@ app.post(securedpath + '/getPtoRequestdetailsforManager', supportCrossOriginScri
 });
 
 //Author: Prakash Code Starts for Employee Calendar Ends Here
+
+app.get(securedpath + '/getRoomDetailsNamesList', function (req, res) {
+    res.header("Access-Control-Allow-Origin", "*");
+    var roomKey = url.parse(req.url, true).query['roomKey'];
+    var OrganizationID = url.parse(req.url, true).query['OrganizationID'];
+    pool.getConnection(function (err, connection) {
+        if (err) {
+
+            console.log("Failed! Connection with Database spicnspan via connection pool failed");
+        }
+        else {
+            console.log("Success! Connection with Database spicnspan via connection pool succeeded");
+            connection.query("set @roomKey=?; set@OrganizationID=?; call usp_getRoomDetailsNamesList(@roomKey,@OrganizationID)", [roomKey,OrganizationID], function (err, rows) {
+                if (err) {
+                    console.log("Problem with MySQL" + err);
+                }
+                else {
+
+                    res.end(JSON.stringify(rows[2]));
+                }
+            });
+        }
+        connection.release();
+    });
+});
 /*************END MIGRATE CODE**********************************************************/
 //handle generic exceptions
 //catch all other resource routes that are not defined above
