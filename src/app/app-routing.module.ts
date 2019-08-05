a692240c3bbe90b608951fcb0c88f82a0e988e49
prakashs113 <prakash.s@troodontechnology.com>
2019-08-05 10:49:46 +0530

    QR Code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     loadChildren: './control-and-view/reviews/reviews.module#ReviewsModule' 
   },
   {
-    path: 'UserWorkRequest/:Facility_Key/:Floor_Key/:Zone_Key/:rev_orgid/:room_key',// user request page
+    path: 'UserWorkRequest/:Facility_Key/:Floor_Key/:Zone_Key/:RoomType_Key/:rev_orgid/:room_key',// user request page
     loadChildren: './control-and-view/user-work-request/user-work-request.module#UserWorkRequestModule' 
   },
 ];
