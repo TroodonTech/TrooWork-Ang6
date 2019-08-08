578fd79dca9772b11632a75e5555ebc562931e06
VarunRajSS <varun.troodon@outlook.com>
2019-08-08 12:22:38 +0530

    user work request stlying by Rodney

--- src/app/control-and-view/user-work-request/user-work-request.module.ts
+++ src/app/control-and-view/user-work-request/user-work-request.module.ts
@@ -3,26 +3,25 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { NgDatepickerModule} from 'ng2-datepicker';
-
+import { NgDatepickerModule } from 'ng2-datepicker';
 import { UserWorkRequestComponent } from "./user-work-request.component";
-
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 const routes: Routes = [
   {
     path: '',
     component: UserWorkRequestComponent
   }
-  
+
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     HttpClientModule,
-
     NgDatepickerModule,
     FormsModule, ReactiveFormsModule,
-    RouterModule.forChild(routes)
+    RouterModule.forChild(routes),
+    MDBBootstrapModule.forRoot()
   ],
   declarations: [UserWorkRequestComponent]
 })
