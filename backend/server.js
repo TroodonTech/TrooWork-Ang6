e7e5846c7abb0e407289ea3ed7421c5477761930
prakashs113 <prakash.s@troodontechnology.com>
2020-09-09 15:29:36 +0530

    Inspection Complete style issue, scheduler collapse for MCP, Scheduler Date issue Fix

--- backend/server.js
+++ backend/server.js
@@ -15635,7 +15635,7 @@ app.post(securedpath + '/getprofileimgapi', function (req, res) {
 
     var employeeKey = req.body.empid;
     var OrganisationId = req.body.orgid;
-    var imgid = req.body.imgid;
+    var imgid = 1;
     //var mDate = req.body.maintdate;
 
     pool.getConnection(function (err, connection) {
@@ -19350,7 +19350,7 @@ app.get(securedpath + '/employeesForScheduler_SuType', function (req, res) {
 
                     if (OrganizationID == 103) {
                         for (var j = 0; j <= arr; j++) {// inserting array value to scheduler tree list
-                            resources.push({ name: tempArr[j][0].Description, id: tempArr[j][0].Idemployeegrouping, "expanded": true, children: tempArr[j], IsShift: 1, backColor: tempArr[j][0].backColor });
+                            resources.push({ name: tempArr[j][0].Description, id: tempArr[j][0].Idemployeegrouping, "expanded": false, children: tempArr[j], IsShift: 1, backColor: tempArr[j][0].backColor });
 
                         }
                     } else {
@@ -21556,7 +21556,7 @@ app.get(securedpath + '/employeesrowFiltering', function (req, res) {
                     if (tempArr.length > 0) {
                         if (OrganizationID == 103) {
                             for (var j = 0; j <= arr; j++) {// inserting array value to scheduler tree list
-                                resources.push({ name: tempArr[j][0].Description, id: tempArr[j][0].Idemployeegrouping, "expanded": true, children: tempArr[j], IsShift: 1, backColor: tempArr[j][0].backColor });
+                                resources.push({ name: tempArr[j][0].Description, id: tempArr[j][0].Idemployeegrouping, "expanded": false, children: tempArr[j], IsShift: 1, backColor: tempArr[j][0].backColor });
 
                             }
                         } else {
